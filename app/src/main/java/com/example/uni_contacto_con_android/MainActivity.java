8a5a0b01df16f3fc4a6a6b00cc6b571d0c204db9
David <dsjgdavid@gmail.com>
2020-01-30 19:11:09 +0100

    Primer Commit

---
+++ app/src/main/java/com/example/uni_contacto_con_android/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.uni_contacto_con_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
